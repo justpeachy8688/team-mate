93036978f38aa140b675bccbb1526d99fd66aa3c
Taylor Sanders <tsanders3515@gmail.com>
2021-02-28 13:04:43 -0900

    first questions

--- index.js
+++ index.js
@@ -1,2 +1,31 @@
+//Include packages needed for this assignment
 const inquirer = require('inquirer');
 const jest = require('jest');
+
+const questions = () =>
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: "What is the team manager's name?",
+        },
+        {
+            type: 'input',
+            name: 'employeeId',
+            message: "What is the team member's employee ID?",
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: "What is the team member's email?",
+        },
+        {
+            type: 'input',
+            name: 'office',
+            message: "What is the team member's office number?",
+        }
+    ]);
+
+const generateHTML = (answers) => {
+
+};
